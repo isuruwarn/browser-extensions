736cff3cb2bf83a3d9993f56dedf27abac6652b8
Isuru Warnakulasooriya <isuru.warnakulasooriya@sap.com>
2019-11-04 07:28:01 -0800

    fix for youtube content script

--- chrome/browser-utils/contentScript-youtube.js
+++ chrome/browser-utils/contentScript-youtube.js
@@ -3,12 +3,15 @@ console.log('Running content script for youtube url - ' + url);
 
 var hide_youtube_related_vids = false;
 chrome.storage.sync.get('hide_youtube_related_vids', function(data) {
-	
 	hide_youtube_related_vids = data.hide_youtube_related_vids;
 	console.log('hide_youtube_related_vids=' + hide_youtube_related_vids);
-	
-	if( hide_youtube_related_vids ) {
-		console.log('Removing related youtube videos section');
+});
+
+document.addEventListener("DOMNodeInserted", function(e) {
+
+	if( hide_youtube_related_vids && ( e.target.nodeName == "div" || e.target.nodeName == "DIV" ) && document.getElementById("related") ) {
+		console.log('Removing related youtube videos section' );
 		document.getElementById("related").remove();
 	}
-});
+
+}, false );
--- firefox/browser-utils/contentScript-youtube.js
+++ firefox/browser-utils/contentScript-youtube.js
@@ -4,9 +4,13 @@ console.log('Running content script for youtube url - ' + url);
 var getting_hide_youtube_related_vids = browser.storage.sync.get('hide_youtube_related_vids');
 getting_hide_youtube_related_vids.then( (res) => {
 	console.log('hide_youtube_related_vids=' + res.hide_youtube_related_vids);
-	if( res.hide_youtube_related_vids == true ) {
-		console.log('Removing related youtube videos section');
-		document.getElementById("related").remove();
-	}
 });
 
+document.addEventListener("DOMNodeInserted", function(e) {
+
+	if( hide_youtube_related_vids && ( e.target.nodeName == "div" || e.target.nodeName == "DIV" ) && document.getElementById("related") ) {
+		console.log('Removing related youtube videos section' );
+		document.getElementById("related").remove();
+	}
+
+}, false );
